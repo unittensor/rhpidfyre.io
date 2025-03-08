3f48a19b228e0b2d1b207a99a9ff22bb15213141
rhpidfyre <brandon@rhpidfyre.io>
2025-03-08 18:26:18 -0500

    desktop dir

--- src/rt/rfwfs/fs/root.ts
+++ src/rt/rfwfs/fs/root.ts
@@ -5,28 +5,14 @@ import rfwfs from "../main"
 const time_now = (Date.now()/1000)|0
 
 // ------------ Home ------------
-// /home/user/.config
-const config = rfwfs.directory(".config", Permissions.rw, time_now)
-
-// /home/user/.local
-const local = rfwfs.directory(".local", Permissions.rw, time_now)
-
-// /home/user/Downloads
+const config    = rfwfs.directory(".config",   Permissions.rw, time_now)
+const local     = rfwfs.directory(".local",    Permissions.rw, time_now)
 const downloads = rfwfs.directory("Downloads", Permissions.rw, time_now)
+const pictures  = rfwfs.directory("Pictures",  Permissions.rw, time_now)
+const desktop   = rfwfs.directory("Desktop",   Permissions.rw, time_now)
+const videos    = rfwfs.directory("Videos",    Permissions.rw, time_now)
+const music     = rfwfs.directory("Music",     Permissions.rw, time_now)
 
-// /home/user/Pictures
-const pictures = rfwfs.directory("Pictures", Permissions.rw, time_now)
-
-// /home/user/Videos
-const videos = rfwfs.directory("Videos", Permissions.rw, time_now)
-
-// /home/user/Music
-const music = rfwfs.directory("Music", Permissions.rw, time_now)
-
-// /home/user/Desktop
-const desktop = rfwfs.directory("Desktop", Permissions.rw, time_now)
-
-// /home/user
 const user = rfwfs.directory("user", Permissions.r, time_now, [
 	config,
 	local,
@@ -42,7 +28,7 @@ const home = rfwfs.directory("home", Permissions.r, time_now, [user])
 // ------------
 
 // ------------ root ------------
-const bin = rfwfs.directory("bin", Permissions.r, time_now, [])
+const bin = rfwfs.directory("bin", Permissions.r, time_now)
 // ------------
 
 export default rfwfs.directory("/", Permissions.r, time_now, [
