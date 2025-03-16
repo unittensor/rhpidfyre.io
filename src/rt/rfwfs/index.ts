143ac35a993f3b8b429c09721b77a1037821c460
rhpidfyre <brandon@rhpidfyre.io>
2025-03-15 21:30:04 -0400

    fix binary search

--- src/rt/rfwfs/index.ts
+++ src/rt/rfwfs/index.ts
@@ -6,7 +6,7 @@ export default function directory_search<T extends Entry>(entry_collection: T[],
 	let end = entry_collection.length-1
 	while (start<=end) {
 		const median = (start+end)>>1
-		const median_name = entry_collection[median].name
+		const median_name = entry_collection[median].name.inner
 
 		if (median_name === file_name) {
 			return wrap_bsearch(median, entry_collection[median])
