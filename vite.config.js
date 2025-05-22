69dcd51b3cac4e7ec3f1d2d3985038c377ecb2ea
rhpidfyre <brandon@rhpidfyre.io>
2025-05-22 18:21:29 -0400

    add `bcryptjs` and remove `vite-plugin-html`

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
-import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
-	plugins: [
-		createHtmlPlugin({minify: true}),
-	],
 	root: "src",
 	build: {
 		outDir: "../dist"
